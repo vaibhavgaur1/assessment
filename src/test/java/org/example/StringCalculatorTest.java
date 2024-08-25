32f59abb5d1e03aaea2d1f7c73454b07636f2f29
vaibhavgaur1 <vaibhavgaur1499@gmail.com>
2024-08-25 19:17:00 +0530

    Add unit tests for StringCalculator

---
+++ src/test/java/org/example/StringCalculatorTest.java
@@ -0,0 +1,33 @@
+package org.example;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class StringCalculatorTest {
+
+    @Test
+    public void testEmptyString() {
+        assertEquals(0, StringCalculator.add(""));
+    }
+
+    @Test
+    public void testSingleNumber() {
+        assertEquals(1, StringCalculator.add("1"));
+        assertEquals(5, StringCalculator.add("5"));
+    }
+
+    @Test
+    public void testTwoNumbers() {
+        assertEquals(6, StringCalculator.add("1,5"));
+        assertEquals(3, StringCalculator.add("1\n2"));
+    }
+
+    @Test
+    public void testMultipleNumbers() {
+        assertEquals(6, StringCalculator.add("1,2,3"));
+        assertEquals(15, StringCalculator.add("1\n2,3\n4,5"));
+    }
+
+ 
+}
+
